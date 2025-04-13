11a1998b2a41e99beb696d56a3cb0bc8fd03de1d
FacuAuciello <facundoauciello@gmail.com>
2025-04-13 19:45:50 -0300

    actualizando repo

---
+++ 04 Estructuras Repetitivas/Auciello_Facundo_TP4_Repetitivas.py
@@ -0,0 +1,133 @@
+#Ejercicio 1
+
+#for ejercicio1 in range (100 + 1):
+#    print (ejercicio1)
+
+#Ejercicio 2
+
+#numeroUsuario = int (input("Ingresa un numero: "))
+
+#if numeroUsuario == 0:
+#    contador = 1
+#else:
+#    contador = 0
+#
+#while numeroUsuario != 0:
+#    numeroUsuario = numeroUsuario // 10
+#    contador += 1
+#print ("cantidad de digitos: ", contador)
+
+#Ejercicio 3
+
+#numero1 = int (input("Ingresa el primer numero: "))
+#numero2 = int (input("Ingresa el segundo numero: "))
+#contador = 0
+
+#for i in range (numero1 + 1, numero2, + 1):
+#    print (i)
+#    contador += contador
+
+#Ejercicio 4
+
+#contador = 0
+
+#numerosUsuario = int (input ("Ingresa un numero: "))
+#while numerosUsuario != 0:
+#    contador += numerosUsuario
+#    numerosUsuario = int (input ("Ingresa un numero: "))
+    
+
+#print ("El total acumulado es de: ", contador)
+
+#Ejercicio 5
+#import random
+#aleatorio = random.randint (1,9) #Numeros random de 1 a 9, con una cantidad de 9 numeros
+
+#contador = 0
+#gano = False
+
+#while gano != True:
+#    numero_Usuario = int (input ("Ingresa un numero: "))
+#    contador = contador + 1
+#    if numero_Usuario  == aleatorio:
+#        gano = True
+
+#print ("Ganaste!!. Intentos necesarios:", contador )
+
+#Ejercicio 6 
+
+#for i in range (100,-1,-1):
+#    if i % 2 == 0:
+#        print (i)
+
+#Ejercicio 7 
+
+#acumulador = 0
+
+#numeroUsuario = int (input ("Ingresa un numero: "))
+#for i in range (0, numeroUsuario, 1):
+#    acumulador = acumulador + i
+#    print (i)
+
+#print ("La suma es de: ", acumulador )
+
+#Ejercicio 8 
+
+#contadorPar = 0
+#contadorImpar = 0
+#contadorPositivo = 0
+#contadorNegativo = 0
+
+#for i in range (100):
+#    numero = int (input ("Ingresa un numero: "))
+
+#    if numero % 2 == 0:
+#        contadorPar = contadorPar + 1
+#    else:
+#        contadorImpar = contadorImpar + 1
+
+#    if numero >= 0:
+#        contadorPositivo = contadorPositivo + 1
+#    else:
+#        contadorNegativo = contadorNegativo + 1 
+
+#print ("Ingresaste " , contadorPar, "pares" )
+#print ("Ingresaste " , contadorImpar, "impares" )
+#print ("Ingresaste " , contadorPositivo, "positivos" )
+#print ("Ingresaste " , contadorNegativo, "negativos" )
+
+#Ejercicio 9) 
+
+#rango = 10
+#acumulador = 0
+
+#for i in range (rango):
+#    numero = int (input ("Ingresa un numero: "))
+#    acumulador = acumulador + numero
+
+#print ("La media de los valores ingresados es de: ", acumulador / rango)
+
+#10) Ejercicio 10
+
+#numero = input("Ingresa un número: ")
+#numero_invertido = ""
+
+#for digito in numero:
+#    numero_invertido = digito + numero_invertido  
+
+#print("Número invertido:", numero_invertido)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
