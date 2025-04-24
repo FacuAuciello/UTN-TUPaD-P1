486033798920302d9a522060bd0ce0d72b2b78ad
FacuAuciello <facundoauciello@gmail.com>
2025-04-24 11:35:22 -0300

    actualizando repo. Subiendo tp listas

---
+++ 05 Listas/Auciello_Facundo_TP5_Listas.py
@@ -0,0 +1,105 @@
+#Ejercicio 1
+
+#Desde 4, yendo de 4 en 4
+#listaNumerica = list (range (4,101,4))
+#print (listaNumerica)
+
+#Ejercicio 2
+
+#Opcion 1
+#listaEquipos = ["Lakers", "Bulls", "Denver", "Minnesota", "Orlando"]
+#print (listaEquipos[3])
+
+#Opcion 2 - Los indices negativos comienzan en -1 para el ultimo elemento y siguen hacia atras
+#listaEquipos = ["Lakers", "Bulls", "Denver", "Minnesota", "Orlando"]
+#print (listaEquipos [-2])
+
+#Ejercicio 3 .append agregar
+
+#actualizarNombres = []
+#actualizarNombres.append("Facundo")
+#actualizarNombres.append("Nahuel")
+#actualizarNombres.append("Jorge")
+
+#print (actualizarNombres)
+
+#Ejercicio 4 slicing actualizar
+
+#Opcion 1
+#listaAnimales = ["Perro", "Gato", "Conejo", "Pez"]
+#print (listaAnimales)
+
+#listaAnimales [1] = "Loro"
+#listaAnimales [3] = "Oso"
+#print (listaAnimales)
+
+#Opcion 2
+#listaAnimales = ["Perro", "Gato", "Conejo", "Pez"]
+#listaAnimales = ["Perro", "Gato", "Conejo", "Pez"]
+#print (listaAnimales)
+
+#listaAnimales [-1] = "Oso"
+#listaAnimales [-3] = "Loro"
+#print (listaAnimales)
+
+#Ejercicio 5 - .remove elimina un numero ingresado pero al poner max, elimina el mas grande.
+
+#numeros = [8, 15, 3, 22, 7]
+#numeros.remove(max(numeros))
+#print (numeros)
+
+#Ejercicio 6
+
+#listaNumeros = list (range(10,31,5))
+#print (listaNumeros[0:2]) #:2 para incluir al 15 que esta en posicion 1
+
+#Ejercicio 7
+
+#autos = ["Sedan", "Polo", "Suran", "Gol"]
+#print(autos)
+
+#autos[1] = "Peugeot"
+#autos[2] = "Honda"
+#print(autos)
+
+#Ejercicio 8
+
+#dobles = []
+
+#dobles.append(5*2)
+#dobles.append(10*2)
+#dobles.append(15*2)
+#print (dobles)
+
+#Ejercicio 9
+
+#compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
+
+#lista1 = compras[0] #pan y leche (posicion 0)
+#lista2 = compras[1] #arroz, fideos y salsa (posicion 1)
+#lista3 = compras[2] #agua (posicion 2)
+
+#compras[2].append("jugo") 
+#compras[1][1] = "tallarines" 
+#compras[0].remove("pan")
+
+#print (compras)
+
+#Ejercicio 10
+
+#listaAnidada = [15, True, [25.5, 57.9, 30.6], False]
+#print (listaAnidada [3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
